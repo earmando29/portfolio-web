4d34b69fa1bb15320cfd2bcf23b01f52d722409c
earmando29 <earmando29@gmail.com>
2022-11-16 09:32:28 -0600

    Line updated

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TimelineItem({ year, title, duration, details}) {
     return (
-        <ol className="flex flex-col md:flex-row relative border-1 border-stone-200">
+        <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
             <li className="mb-10 ml-4">
                 <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white" />
                 <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
