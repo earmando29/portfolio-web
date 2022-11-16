f96519c4d46345ba780c8eb9df47874876eea26b
earmando29 <earmando29@gmail.com>
2022-11-16 01:08:24 -0600

    png on site

--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Chat App',
-        imgUrl: '',
+        imgUrl: '/assets/chat_app.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: 'https://dontbeshy.netlify.app/',
     },
     {
         title: 'EverGreene',
-        imgUrl: '',
+        imgUrl: '/assets/Evergreene.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: '',
     },
     {
         title: 'SnakeGame',
-        imgUrl: '',
+        imgUrl: '/assets/snake_game.png',
         stack: ['C++'],
         link: '',
     }
