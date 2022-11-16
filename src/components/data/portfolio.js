d0a895e9761a4d15896bbf7e0da7e8817bba486f
earmando29 <earmando29@gmail.com>
2022-11-16 00:58:17 -0600

    Portfolio Base

---
+++ src/components/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Chat App',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://dontbeshy.netlify.app/',
+    },
+    {
+        title: 'EverGreene',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: '',
+    },
+    {
+        title: 'SnakeGame',
+        imgUrl: '',
+        stack: ['C++'],
+        link: '',
+    }
+]
