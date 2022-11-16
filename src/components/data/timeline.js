78b59222dc08c9c086a7cda81eec8f8b80d43035
earmando29 <earmando29@gmail.com>
2022-11-16 09:27:29 -0600

    Timeline Added

---
+++ src/components/data/timeline.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        year: 'Today',
+        title: 'University of Houston',
+        duration: '2 years',
+        details: 'I have little to say',
+    },
+    {
+        year: '2016',
+        title: 'Langham Creek High School',
+        duration: '4 years',
+        details: 'Varsity soccer district champs',
+    }
+]
