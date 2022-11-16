ef7d272a7d3f4307afd4360bda71dd86910689da
earmando29 <earmando29@gmail.com>
2022-11-16 10:31:09 -0600

    Added urls

--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -2,19 +2,19 @@ export default [
     {
         title: 'Chat App',
         imgUrl: '/assets/chat_app.png',
-        stack: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://dontbeshy.netlify.app/',
+        stack: ['JavaScript', 'React.js', 'Socket.io'],
+        link: 'https://dontbeshy.netlify.app',
     },
     {
         title: 'EverGreene',
         imgUrl: '/assets/Evergreene.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
-        link: '',
+        link: 'nothing',
     },
     {
         title: 'SnakeGame',
         imgUrl: '/assets/snake_game.png',
         stack: ['C++'],
-        link: '',
+        link: 'nothing',
     }
 ]
