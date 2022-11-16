3cacbfa51d70dcd6f7b0ab853473a19bb0612405
earmando29 <earmando29@gmail.com>
2022-11-16 09:51:06 -0600

    Footer Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Footer() {
     return (
-        <div></div>
+        <div className="py-5 text-center">
+            {/*social icons*/}
+            <p className="text-sm mt-2 opacity-50">
+                &copy; {new Date().getFullYear()} Armando
+                Espinoza. All rights reserved.
+            </p>
+        </div>
     )
 }
 
