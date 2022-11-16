1648c2c640c1cebc133bf793028ba94b16da5ea9
earmando29 <earmando29@gmail.com>
2022-11-16 13:20:49 -0600

    updated timeline desc

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Armando</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer Student</p>
+            {/* <img src={imgUrl}
+                alt="portfolio"
+                className="w-half h-36 md:h-48 object-cover rounded-md" 
+                /> */}
+            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Armando Espinoza</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Developer/Engineer Student</p>
             <p className="text-sm max-w-xl mb-6 font-bold">
-                I am Armando Espinoza, 
-                currently studying Computer Science at the University of Houston. 
+                I am Armando Espinoza pursuing a Computer Science Major at the University of Houston. 
                 <br />
-                Expected graduation date is May 2024
+                My expected graduation date is May 2024.
+                <br />
+                Currently in search of a internship for Summer 2023!
             </p>
         </div>
     )
--- src/components/data/timeline.js
+++ src/components/data/timeline.js
@@ -2,8 +2,14 @@ export default [
     {
         year: 'Today',
         title: 'University of Houston',
+        duration: '2 months',
+        details: 'Bachelor of Science in Computer Science. Relavent courses: Data structures, Computer Organization',
+    },
+    {
+        year: '2020',
+        title: 'Lone Star Community College',
         duration: '2 years',
-        details: 'I have little to say',
+        details: 'Studying Computer Science. Relavent courses: OOP (Object Oriented Programming)',
     },
     {
         year: '2016',
