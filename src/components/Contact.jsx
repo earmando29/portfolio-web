335daf1098955b7fedf88b17ce8dcc2c27f4afe0
earmando29 <earmando29@gmail.com>
2022-11-17 01:06:00 -0600

    added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
         <div className="flex flex-col mb-10 mx-auto">
             <div className="flex justify-center items-center">
                 <form 
-                    action="https://getform.io/f/88c41578-7f9a-4354-831c-8eb32a6bb15d"
+                    action="https://formsubmit.co/earmando29@gmail.com"
                     method="POST"
                     className="flex flex-col w-full md:w-7/12"
                     >
@@ -29,7 +29,7 @@ function Contact() {
                         rows="10"
                         className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                     />
-                    <button type="button"
+                    <button type="submit"
                         className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
                         >
                         Contact Me
