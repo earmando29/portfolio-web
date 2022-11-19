9d22220ac02e315dbe91c973d77543617f61fbb6
earmando29 <earmando29@gmail.com>
2022-11-19 00:35:54 -0600

    added dependencies

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Fragment } from 'react'
+import { Link } from "react-router-dom";
+import Scroll from 'react-scroll'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
@@ -36,7 +38,8 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-stone-800">
+    <Disclosure as="nav" 
+        className="bg-stone-800">
         {({ open }) => (
         <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -46,9 +49,9 @@ export default function Example() {
                     <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                         <span className="sr-only">Open main menu</span>
                         {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                        ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                            <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                            ) : (
+                            <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                         )}
                     </Disclosure.Button>
                     </div>
