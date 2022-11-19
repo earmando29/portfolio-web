553ed06e9507e527c012781fb4d847cbae6736cc
earmando29 <earmando29@gmail.com>
2022-11-19 15:35:11 -0600

    updated mobile nav bar scrolling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -70,7 +70,7 @@ export default function Example() {
                                         spy={true} 
                                         smooth={true} 
                                         offset={-100} 
-                                        duration={500}
+                                        duration={600}
                                         className={classNames(
                                             item.current ? 'bg-gray-900 text-white' : 'text-stone-800 dark:text-white hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium'
                                         )}
@@ -88,18 +88,27 @@ export default function Example() {
             <Disclosure.Panel className="sm:hidden">
                 <div className="space-y-1 px-2 pt-2 pb-3">
                     {navigation.map((item) => (
-                    <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-stone-800 dark:text-white hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
+                    <Link
+                        activeClass="active" 
+                        to={item.href} 
+                        spy={true} 
+                        smooth={true} 
+                        offset={-275} 
+                        duration={900}
                     >
-                        {item.name}
-                    </Disclosure.Button>
+                        <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            // href={item.href}
+                            className={classNames(
+                            item.current ? 'bg-gray-900 text-white' : 'text-stone-800 dark:text-white hover:bg-gray-700 hover:text-white',
+                            'block px-3 py-2 rounded-md text-base font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                        >
+                                {item.name}
+                        </Disclosure.Button>
+                    </Link>
                     ))}
                 </div>
             </Disclosure.Panel>
